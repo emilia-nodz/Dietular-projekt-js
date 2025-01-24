ffee1d6154814fc5d45d58e9419d798a811f508a
emilia-nodz <extra.maslo.game@gmail.com>
2025-01-24 01:10:19 +0100

    dodanie kalendarza

---
+++ frontend/src/app/calendar/calendar.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, model } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+@Component({
+  selector: 'app-calendar',
+  standalone: true,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatCardModule, MatDatepickerModule, MatNativeDateModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  templateUrl: './calendar.component.html',
+  styleUrl: './calendar.component.css'
+})
+export class CalendarComponent {
+  selected = model<Date | null>(null);
+
+}
