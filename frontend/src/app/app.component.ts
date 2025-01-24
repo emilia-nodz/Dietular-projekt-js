0db57d53dcb469e224b6bb7e70328c89d316edb5
emilia-nodz <extra.maslo.game@gmail.com>
2025-01-24 01:10:00 +0100

    dodanie kalendarza

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from "./layout/header/header.component";
+import {MatCardModule} from '@angular/material/card';
+import {MatNativeDateModule, provideNativeDateAdapter} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink, RouterLinkActive, CommonModule, HeaderComponent, RouterModule],
+  imports: [RouterOutlet,RouterLink, RouterLinkActive, CommonModule, HeaderComponent, RouterModule, MatCardModule, MatDatepickerModule, MatNativeDateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
